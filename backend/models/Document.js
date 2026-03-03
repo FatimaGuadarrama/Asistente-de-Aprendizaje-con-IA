b3acf9b643de8559233fdc707f07fdbd93a30917
Fátima Guadarrama <al07085200@tecmilenio.mx>
2026-03-03 17:22:11 -0600

    Ultimos cambios

--- backend/models/Document.js
+++ backend/models/Document.js
@@ -15,7 +15,7 @@ const documentSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    fileUrl: {
+    fileURL: {
         type: String,
         required: true,
     },
